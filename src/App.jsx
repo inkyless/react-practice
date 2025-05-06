d621504e507fdb953ccbaa82d7344f46261c27c0
inkyless <violalakwok@gmail.com>
2025-05-06 16:07:38 +0700

    Conditional Listing Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Drink({ name }) {
+  let part, caffeine, age;
+  if (name === "Tea"){
+    part = "leaf";
+    caffeine = "15-70 mg/cup";
+    age = "4,000+ years";
+  } else if (name === "Coffee"){
+    part = "bean";
+    caffeine = "80-185 mg/cup";
+    age = "1,000+ years";
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section>
+      <h1>{name}</h1>
+      <dl>
+          <dt>Part of plant</dt>
+          <dd>{part}</dd>
+          <dt>Caffeine content</dt>
+          <dd>{caffeine}</dd>
+          <dt>Age</dt>
+          <dd>{age}</dd>
+      </dl>
+
+    </section>
+  );
 }
 
-export default App
+export default function DrinkList() {
+  return (
+    <div>
+      <Drink name="Tea" />
+      <Drink name="Coffee" />
+    </div>
+  );
+}
